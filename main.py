45b08c7928ff7f8f2ee3ebe196807a6887cd82fe
Ivette <car16020@uvg.edu.gt>
2019-07-30 14:44:38 -0600

    Login, add plugins and menu

--- main.py
+++ main.py
@@ -1,69 +1,138 @@
-import asyncio
-from argparse import ArgumentParser
-from getpass import getpass
+import sys
 import logging
-from slixmpp import  ClientXMPP
-from slixmpp.exceptions import IqError, IqTimeout
+import getpass
+import sleekxmpp
+import time
+import ssl
 
-class client_xmpp(ClientXMPP):
-  def __init__(self, jid, password):
-    super().__init__(jid, password)
+from optparse import OptionParser
+from sleekxmpp.exceptions import IqError, IqTimeout
+from menu import *
 
-    self.add_event_handler("session_start", self.start)
-    self.add_event_handler("register", self.register)
+class EchoBot(sleekxmpp.ClientXMPP):
 
-  def start(self, event):
-    self.send_presence()
-    self.get_roster()
-    self.disconnect()
+    def __init__(self, jid, password, optmen):
 
-  async def register(self, iq):
-    resp = self.Iq()
-    resp['type'] = 'set'
-    resp['register']['username'] = self.boundjid.user
-    resp['register']['password'] = self.password
+        sleekxmpp.ClientXMPP.__init__(self, jid, password)
+        #Evento de start
+        print("The option is: ", optmen)
+        if(optmen == 1): 
+          self.add_event_handler("session_start", self.start)
+          self.add_event_handler("register", self.register)
+        elif(optmen == 2):
+          self.add_event_handler("session_start", self.start)
+          self.add_event_handler("message", self.message)
+  
 
-    try:
-      await resp.send()
-      logging.info("Account created for %s!" %self.boundjid)
-    except IqError as e:
-      logging.error("ERROR: user not created %s" % e.iq['error']['text'])
-      self.disconnect()
-    except IqTimeout:
-      loging.error("ERROR: No response")
-      self.disconnect()
+    #Process event: session_start
+    def start(self, event):
+        print('Session start')
+        self.send_presence()
+        self.get_roster()
 
-#MAIN
-if __name__ == "__main__":
-  parser = ArgumentParser()
+    #Receive message
+    def message(self, msg):
+        if msg['type'] in ('chat', 'normal'):
+            msg.reply("Sent\n%(body)s" % msg).send()
+            print(msg)
 
-  parser.add_argument("-q", "--quiet", help="set logging to ERROR",
-                        action="store_const", dest="loglevel",
-                        const=logging.ERROR, default=logging.INFO)
-  parser.add_argument("-d", "--debug", help="set logging to DEBUG",
-                      action="store_const", dest="loglevel",
-                      const=logging.DEBUG, default=logging.INFO)
+    def register(self, iq):
+        resp = self.Iq()
+        resp['type'] = 'set'
+        resp['register']['username'] = self.boundjid.user
+        resp['register']['password'] = self.password
+        try:
+            resp.send(now=True)
+            logging.info("Register succesfull: %s!" % self.boundjid)
+        except IqError as e:
+            logging.error("Error: unable to register %s" %
+                    e.iq['error']['text'])
+            self.disconnect()
+        except IqTimeout:
+            logging.error("Error: no response from server")
+            self.disconnect()
 
-  parser.add_argument("-j", "--jid", dest = "jid", help = "Define JID")
-  parser.add_argument("-p", "--password", dest = "password", help = "Write down your password")
+    #Get all users and print them
+    def get_users(self):
+        print(self.get_roster())
 
-  args = parser.parse_args()
+    def delete_user(self):
+        resp = self.Iq()
+        resp['type'] = 'set'
+        resp['from'] = self.boundjid.user
+        resp['register'] = ' '
+        resp['register']['remove'] = ' '
+        try:
+            resp.send(now=True)
+            print("Account deleted for %s!" % self.boundjid)
+        except IqError as e:
+            logging.error("Could not delete account: %s" %
+                    e.iq['error']['text'])
+            self.disconnect()
+        except IqTimeout:
+            logging.error("No response from server.")
+            self.disconnect()
+    
 
-  logging.basicConfig(level=args.loglevel,format='%(levelname)-8s %(message)s')
+if __name__ == '__main__':
+    optp = OptionParser()
 
-  if args.jid is None:
-    args.jid = input("Username: ") + "@alumchat.xyz"
-  if args.password is None:
-    args.password = getpass("Password: ")
+    #Verbose
+    optp.add_option('-q', '--quiet', help='set logging to ERROR',
+                    action='store_const', dest='loglevel',
+                    const=logging.ERROR, default=logging.INFO)
+    optp.add_option('-d', '--debug', help='set logging to DEBUG',
+                    action='store_const', dest='loglevel',
+                    const=logging.DEBUG, default=logging.INFO)
+    optp.add_option('-v', '--verbose', help='set logging to COMM',
+                    action='store_const', dest='loglevel',
+                    const=5, default=logging.INFO)
 
-  xmpp = client_xmpp(args.jid, args.password)
+    #Information: jid, password, to, message
+    optp.add_option("-j", "--jid", dest="jid",
+                    help="JID to use")
+    optp.add_option("-p", "--password", dest="password",
+                    help="password to use")
+    optp.add_option("-t", "--to", dest="to",
+                    help="JID to send the message to")
+    optp.add_option("-m", "--message", dest="message",
+                    help="message to send")
 
-  xmpp.register_plugin('xep_0030') # Service Discovery
-  xmpp.register_plugin('xep_0004') # Data forms
-  xmpp.register_plugin('xep_0066') # Out-of-band Data
-  xmpp.register_plugin('xep_0077') # In-band Registration
+    opts, args = optp.parse_args()
 
-  xmpp.connect()
-  xmpp.process()
+    #Login
+    logging.basicConfig(level=opts.loglevel,
+                        format='%(levelname)-8s %(message)s')
 
-  pass
+    """
+    if opts.jid is None:
+        opts.jid = input("Username: ")
+    if opts.password is None:
+        opts.password = getpass.getpass("Password: ")
+    if opts.to is None:
+        opts.to = input("Send To: ")
+    if opts.message is None:
+        opts.message = input("Message: ")
+    """
+    optmen = int(menu())
+    xmpp = EchoBot(opts.jid, opts.password, optmen)
+    
+    #Register plugins
+    xmpp.register_plugin('xep_0004') # Data forms
+    xmpp.register_plugin('xep_0030') # Service Discovery
+    xmpp.register_plugin('xep_0045') # Multichat
+    xmpp.register_plugin('xep_0060') # PubSub
+    xmpp.register_plugin('xep_0066') # Out-of-band Data
+    xmpp.register_plugin('xep_0077') # In-band Registration
+    xmpp.register_plugin('xep_0199') # Ping
+    xmpp['xep_0077'].force_registration = True
+
+    #Server connection
+    #if xmpp.connect():
+    if xmpp.connect(('alumchat.xyz', 5222)):
+        xmpp.process(block=True) #or false? 
+        print("Done")
+        #Second menu
+       
+    else:
+        print("Unable to connect.")
---
+++ menu.py
@@ -0,0 +1,9 @@
+
+def menu():
+    print("WELCOME")
+    print("OPTIONS")
+    print("1. Register")
+    print("2. Log in")
+    op = input("Select and option")
+    return op
+    
